914909d4a294cb05fdeb021d5fbcf4bebb106449
DARIONZITA <92880647+DARIONZITA@users.noreply.github.com>
2021-10-21 21:11:43 +0100

    Add files via upload
    criados atributos estacionais  de nome e idade

--- pessoa.py
+++ pessoa.py
@@ -1,7 +1,15 @@
 class Pessoa:
+    def __init__(self,nome=None,idade=35):
+            self.nome=nome
+            self.idade=idade
     def cumprimentar(self):
         return 'ola'
 if __name__ == '__main__':
-    p= Pessoa()
+    p= Pessoa('dario')
     print(Pessoa.cumprimentar(p))
-    print(p.cumprimentar())
+    print(p.nome)
+    p.nome='dario nzita'
+    print(p.nome)
+    print(p.idade)
+    o=Pessoa('paulo',13)
+    print(o.idade)
